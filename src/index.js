4a9564d0d3c6756291b2b0b060463dc03c6569ec
guin0019 <amaury.guindon@etudiant.univ-reims.fr>
2023-06-12 15:34:18 +0200

    backOfficeVeto + Nav

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./error-page";
+import NavBar from './components/nav/nav';
 import Login from './components/login/Login';
+import BackOfficeVeto from './components/backOfficeVeto/backOfficeVeto'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BackOfficeVeto />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <div>
+      <div className='homeNav'>
+        <NavBar />
+      </div>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
 
