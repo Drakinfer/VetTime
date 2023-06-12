dd7189e7a5c8ed3b39903a653091390b088550e4
MAROUAN MAKIL <marouan.makil@reseau.sncf.fr>
2023-06-12 12:32:22 +0200

    signup

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import Signup from '../signup/Signup';
 import logo from '../../assets/images/logovet.png';
 import './Login.css';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signUp, setSignUp] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -14,37 +16,53 @@ const LoginPage = () => {
     setPassword(event.target.value);
   };
 
+  const handleSignup = () => {
+    setSignUp(true);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div className="login-page">
-      <img src={logo} id='logo'/>
-      <h1>Connexion</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Mot de passe</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button type="submit">Se connecter</button>
-      </form>
+    <div>
+      {signUp ? 
+
+
+      <Signup/>
+
+      :       
+      
+        <div className="login-page">
+        <img src={logo} id='logo'/>
+        <h1>Connexion</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder='monemail@gmail.com'
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <button type="submit">Se connecter</button>
+          <button id="signup" onClick={handleSignup}>Inscription</button>
+        </form>
+      </div>
+    }
     </div>
   );
 };
---
+++ src/components/signup/Signup.js
@@ -0,0 +1,171 @@
+import React, { useState } from 'react';
+import logo from '../../assets/images/logovet.png'
+import './Signup.css';
+
+const Signup = () => {
+  const [step, setStep] = useState(1);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [city, setCity] = useState('');
+  const [address, setAddress] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
+
+  const handlePreviousStep = () => {
+    setStep(step - 1);
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handlePostalCodeChange = (event) => {
+    setPostalCode(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Ici, vous pouvez ajouter la logique pour traiter la soumission du formulaire
+    // par exemple, en envoyant les données d'inscription au serveur
+  };
+
+  return (
+    <div className="signup-page">
+      <img id="logo" src={logo}/>
+      <h1>Inscription - Étape {step}</h1>
+      {step === 1 && (
+        <form onSubmit={handleNextStep}>
+          <div className="form-group">
+            <label htmlFor="firstName">Prénom</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={handleFirstNameChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Nom</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={handleLastNameChange}
+              required
+            />
+          </div>
+          <button type="submit">Suivant</button>
+        </form>
+      )}
+      {step === 2 && (
+        <form onSubmit={handleNextStep}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <button type="button" onClick={handlePreviousStep}>
+            Précédent
+          </button>
+          <button type="submit">Suivant</button>
+        </form>
+      )}
+      {step === 3 && (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="city">Ville</label>
+            <input
+              type="text"
+              id="city"
+              value={city}
+              onChange={handleCityChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Adresse</label>
+            <input
+              type="text"
+              id="address"
+              value={address}
+              onChange={handleAddressChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="postalCode">Code postal</label>
+            <input
+              type="text"
+              id="postalCode"
+              value={postalCode}
+              onChange={handlePostalCodeChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Téléphone</label>
+            <input
+              type="tel"
+              id="phone"
+              value={phone}
+              onChange={handlePhoneChange}
+              required
+            />
+          </div>
+          <button type="button" onClick={handlePreviousStep}>
+            Précédent
+          </button>
+          <button type="submit">S'inscrire</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Signup;
