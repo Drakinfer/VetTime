048c619f3a5b9648bc919d8a04df740481602860
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2023-06-13 16:33:43 +0200

    adding HomePage

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './components/login/Login';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+  </Router>
+    
 );
