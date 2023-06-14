bd3388623633c2521604c00528734d1d0ee80edb
guin0019 <amaury.guindon@etudiant.univ-reims.fr>
2023-06-14 14:13:07 +0200

    ajout option annuler + filtre date

--- src/components/RendezVousVeto/rendezVousVeto.js
+++ src/components/RendezVousVeto/rendezVousVeto.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import './rendezVousVeto.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import DateFilter from '../dateFilter/dateFilter';
+import Tooltip from '@mui/material/Tooltip';
 
 const rendezVousVetoPage = () => {
 
@@ -13,25 +17,45 @@ const rendezVousVetoPage = () => {
           <span className='h3'>Mes Rendez-Vous</span>
         </div>
         <div className='col-12'>
+          <div className='mt-3 mb-3'>
+            <DateFilter></DateFilter>
+          </div>
+
           <div className='row width mb-2'>
             <div className='col-4 center h5'>Prénom</div>
             <div className='col-4 center h5'>Date</div>
-            <div className='col-4 center h5'>Animal</div>
+            <div className='col-3 center h5'>Animal</div>
+          </div>
+          <div className='row test1 form-control form-control-lg mb-2 col-12'>
+            <div className='col-4 center'><em>test</em></div>
+            <div className='col-4 center'><em>test</em></div>
+            <div className='col-3 center'><em>test</em></div>
+            <Tooltip title="Annuler">
+              <div className='col-1 icon center' >
+                <FontAwesomeIcon icon={faXmark} />
+              </div>
+            </Tooltip>
+
           </div>
           <div className='row test1 form-control form-control-lg mb-2'>
             <div className='col-4 center'><em>test</em></div>
             <div className='col-4 center'><em>test</em></div>
-            <div className='col-4 center'><em>test</em></div>
-          </div>
-          <div className='row test1 form-control form-control-lg mb-2'>
-            <div className='col-4 center'><em>test</em></div>
-            <div className='col-4 center'><em>test</em></div>
-            <div className='col-4 center'><em>test</em></div>
+            <div className='col-3 center'><em>test</em></div>
+            <Tooltip title="Annuler">
+              <div className='col-1 icon center' >
+                <FontAwesomeIcon icon={faXmark} />
+              </div>
+            </Tooltip>
           </div>
           <div className='row test1 form-control form-control-lg'>
             <div className='col-4 center'><em>test</em></div>
             <div className='col-4 center'><em>test</em></div>
-            <div className='col-4 center'><em>test</em></div>
+            <div className='col-3 center'><em>test</em></div>
+            <Tooltip title="Annuler">
+              <div className='col-1 icon center' >
+                <FontAwesomeIcon icon={faXmark} />
+              </div>
+            </Tooltip>
           </div>
         </div>
       </div>
