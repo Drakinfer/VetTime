c7252924513de6b6e071dfa16d1554d52f8abab9
MAROUAN MAKIL <marouan.makil@reseau.sncf.fr>
2023-06-14 14:49:41 +0200

    index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Login from './components/login/Login';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import FormCabinet from './components/form-infos-cabinet/Formcabinet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/infocabinet" element={<FormCabinet/>}/>
     </Routes>
   </Router>
     
